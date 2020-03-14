5bee8961e4dfa8f1fdb5717801df55f095d57534
Mwesigwa David <mwesigwadave10@gmail.com>
2020-03-14 16:26:21 +0300

    max.py

---
+++ max.py
@@ -0,0 +1,12 @@
+def max_number(x,y,z):
+    if x>=y and x>=z:
+        print(x)
+    if y>=x and y>=z:
+        print(y)
+    if z>=x and z>=y:
+        print(z)
+
+
+max_number(9,2,6)
+max_number(6,3,5)
+max_number(6,3,32)
