3988025bd3356ed382c404d79e924216f9e61774
Mwesigwa David <mwesigwadave10@gmail.com>
2020-03-16 21:33:35 +0300

    dict and max

--- max.py
+++ max.py
@@ -1,11 +1,5 @@
 def max_number(x,y,z):
-    if x>=y and x>=z:
-        print(x)
-    if y>=x and y>=z:
-        print(y)
-    if z>=x and z>=y:
-        print(z)
-
+   print(max(x,y,z))
 
 max_number(9,2,6)
 max_number(6,3,5)
